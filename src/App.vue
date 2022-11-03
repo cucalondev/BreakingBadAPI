ff7f76153516c4da42677a658a50ba3f2a193c80
Aphans <rapperanqeel@hotmail.com>
2022-11-04 00:03:50 +0100

    Adding image to list

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,25 @@
 <template>
   <div>
-    <h1>The Breaking Bad</h1>
-
-    <table>
-      <tbody>
-        <tr v-for="todo in result" :key="todo.char_id">
-          {{
-            todo.name
-          }}
-        </tr>
-      </tbody>
-    </table>
+    <ListaPersonajes />
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import ListaPersonajes from "./components/ListaPersonajes.vue";
 export default {
-  data: () => ({
-    result: null,
-  }),
-  mounted() {
-    this.getTodos();
-  },
-  methods: {
-    getTodos() {
-      axios
-        .get("https://www.breakingbadapi.com/api/characters/1")
-        .then((response) => {
-          console.log(response);
-          this.result = response.data;
-        })
-        .catch((e) => console.log(e));
-    },
+  data: () => ({}),
+  components: {
+    ListaPersonajes,
   },
 };
 </script>
-
-<style scoped lang="scss"></style>
+<style>
+img {
+  height: 150px;
+  width: 100px;
+  object-fit: cover;
+  margin-right: 10px;
+  border: 2px solid black;
+  border-radius: 3px;
+}
+</style>
---
+++ src/components/ListaPersonajes.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="todo in result" :key="todo.char_id">
+          <img :src="todo.img" />
+          {{
+            todo.name
+          }}
+          <br />
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  mounted() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      axios
+        .get("https://www.breakingbadapi.com/api/characters/")
+        .then((response) => {
+          console.log(response);
+          this.result = response.data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
+<style>
+img {
+  height: 150px;
+  width: 100px;
+  object-fit: cover;
+  margin-right: 10px;
+  border: 2px solid black;
+  border-radius: 3px;
+}
+</style>
